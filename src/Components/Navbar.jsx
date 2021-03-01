a9d9acfd0f54700a28196396a559de9febf95d7a
Lee Bennett <LDBennett@gmail.com>
2021-03-01 05:30:11 -0500

    Add resources modal

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,3 @@
-// import { Link, animateScroll as scroll } from "react-scroll";
 import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from "react-scroll"
@@ -9,17 +8,30 @@ import Button from 'react-bootstrap/Button'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import { Document, Page } from 'react-pdf';
 
 import CustomModal from './CustomModal'
+import useWindowSize from './Utils/useWindowSize'
 
 import Logo from './../Assets/superior-closing-logo--icon-only.png'
+import TitleFlyer from './../Assets/title flyer.pdf'
+import MovingTips from './../Assets/p68 Moving Tips.pdf'
+import HomeBuyingProcess from './../Assets/p52 Home Buying Process.pdf'
+import WireFraud from './../Assets/p41 Wire Fraud.pdf'
 
 const Navigation = () => {
 
-  const [show, setShow] = useState(false)
-  const [miniNav, setMiniNav] = useState(false)
+  const size = useWindowSize()
+
+  const [ faqs, setFaqs ] = useState(false)
+  const [ resources, setResources ] = useState(false)
+  const [ resourcesPDF, setResourcesPDF ] = useState(TitleFlyer)
+  const [ resourcesWidth, setResourcesWidth ] = useState()
+  const [ miniNav, setMiniNav ] = useState(false)
   const [ expandedNav, setExpandedNav ] = useState(false)
   const scrollY = useScrollPosition()
+
 
   const NavLink = ({to, children}) => {
     return (
@@ -48,6 +60,37 @@ const Navigation = () => {
     setExpandedNav(!expandedNav)
   }
 
+  useEffect(()=>{
+    if( size.width < 380){
+      console.log("TEST");
+      setResourcesWidth(250)
+    }
+    else if( size.width < 992 ){
+      setResourcesWidth(300)
+    }
+    else{
+      setResourcesWidth(null)
+    }
+  },[ size ])
+
+  const changeResource = (e) => {
+    const pdf = e.target.getAttribute('data-pdf')
+    switch(pdf) {
+      case 'title':
+        setResourcesPDF(TitleFlyer)
+        break;
+      case 'wire':
+        setResourcesPDF(WireFraud)
+        break;
+      case 'moving':
+        setResourcesPDF(MovingTips)
+        break;
+      case 'home-buying':
+      default:
+        setResourcesPDF(HomeBuyingProcess)
+    }
+  }
+
   return(
     <>
     <Navbar expanded={expandedNav} onToggle={handleNavClick} bg="dark" variant="dark" expand="lg" fixed="top" className={`${miniNav ? 'minified' : ''} p-3`} >
@@ -63,21 +106,24 @@ const Navigation = () => {
       <Navbar.Toggle aria-controls="navbar-nav b-0" />
       <Navbar.Collapse className="pb-2 pb-lg-0" id="basic-navbar-nav">
         <Nav>
-          <NavLink to="summary">Summary</NavLink>
           <NavLink to="who-we-are">Who We Are</NavLink>
           <NavLink to="services">Services</NavLink>
           <NavLink to="about">About</NavLink>
           <span
             className="nav-link"
             role="button"
-            onClick={()=>setShow(true)}>FAQs</span>
+            onClick={()=>setFaqs(true)}>FAQs</span>
+          <span
+            className="nav-link"
+            role="button"
+            onClick={()=>setResources(true)}>Resources</span>
         </Nav>
         <a href="mailto:Marino@superiorclosings.com" className="ml-auto mr-3"><FontAwesomeIcon icon="envelope" color="#f86124"/></a>
         <a href="tel:1-412-505-5199" className="mr-3"><FontAwesomeIcon icon="phone" color="#f86124"/></a>
         <Button variant="outline-light" href="https://login.e-closingsecured.com/CompanyID/3Z9DNFED6Y" target="_blank">View My Docs</Button>
       </Navbar.Collapse>
     </Navbar>
-    <CustomModal show={show} handler={setShow}>
+    <CustomModal show={faqs} handler={setFaqs}>
         <h3 className="text-center">What Do I Need For Closing?</h3>
         <hr/>
         <Row>
@@ -97,6 +143,27 @@ const Navigation = () => {
               <ListGroup.Item><FontAwesomeIcon icon="check-circle" color="#f86124"/> Forwarding address</ListGroup.Item>
             </ListGroup>
           </Col>
+        </Row>
+    </CustomModal>
+    <CustomModal show={resources} handler={setResources} size="lg">
+        <h3 className="text-center">Resources</h3>
+        <hr/>
+        <Row className="justify-content-center">
+          <ButtonGroup>
+            <Button onClick={changeResource} data-pdf="title">Title Flyer</Button>
+            <Button onClick={changeResource} data-pdf="wire">Wire Fraud</Button>
+            <Button onClick={changeResource} data-pdf="home-buying">Home Buying Process</Button>
+            <Button onClick={changeResource} data-pdf="moving">Moving Tips</Button>
+          </ButtonGroup>
+        </Row>
+        <hr className="mx-5"/>
+        <Row className="justify-content-center" style={{minHeight: resourcesWidth <= 300 ? "400px" : 800}}>
+          <Document file={resourcesPDF} >
+            <Page pageNumber={1} width={resourcesWidth}/>
+          </Document>
+        </Row>
+        <Row className="justify-content-center">
+          <Button className="justify-self-center" href={resourcesPDF} variant="outline-light" download>Download</Button>
         </Row>
     </CustomModal>
     </>
