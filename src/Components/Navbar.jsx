ae9d98b9ddd421a5e69983360e39ac7e2d643ade
Lee Bennett <LDBennett@gmail.com>
2022-06-07 09:57:20 -0400

    view docs removed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -119,7 +119,6 @@ const Navigation = () => {
         </Nav>
         <a href="mailto:Marino@superiorclosings.com" className="ml-auto mr-3"><FontAwesomeIcon icon="envelope" color="#f86124"/></a>
         <a href="tel:1-412-505-5199" className="mr-3"><FontAwesomeIcon icon="phone" color="#f86124"/></a>
-        <Button variant="outline-light" href="https://login.e-closingsecured.com/CompanyID/3Z9DNFED6Y" target="_blank">View My Docs</Button>
       </Navbar.Collapse>
     </Navbar>
     <CustomModal show={faqs} handler={setFaqs}>
